31c22373c731a4a7bb412272e4916968c27cc1f5
Rampavan123 <Rampavan123>
2021-10-29 12:26:08 +0530

    for testing

---
+++ file_1.py
@@ -0,0 +1,35 @@
+"""
+f=open("pavan.txt",'w')
+f.write("i am a python programmer")
+f.close()
+"""
+"""
+f=open("pavan.txt",'r')
+data=f.read()
+f.close()
+print(data)
+"""
+
+"""
+f=open("pavan.txt",'a')
+f.write("i am aware of jira")
+f.close()
+"""
+#edcba
+##for i in range(1, 6):
+##    for j in range(65, 65+i):
+##        a = chr(j)
+##        print(a, end="")
+##    print()
+##for i in range(1, 6):
+##    #print(i)
+##    for j in range(69, 69-i, -1):
+##        #print(j)
+##        #a = chr(j)
+##        print(chr(j), end="")
+##    #print()
+
+for i in range(65, 70):
+    #for j in range(5,0,-1):
+    print(chr(i))
+    
